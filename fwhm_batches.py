a9d29158db66597e37fd7ae1f805c5b08fad5dc9
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-29 18:02:13 +0000

    include fwhm_x, y

--- fwhm_batches.py
+++ fwhm_batches.py
@@ -33,9 +33,7 @@ def save_results_json(bjd, airmass, pixel_size, fwhm_results):
         regions_data.append({
             "Region": region_name,
             "FWHM": results["FWHM"],
-            "Ratio": results["Ratio"],
-            "FWHM_x": results["FWHM_x"],
-            "FWHM_y": results["FWHM_y"]
+            "Ratio": results["Ratio"]
         })
 
     result_data = {
@@ -138,8 +136,8 @@ def calculate_fwhm(image_data, pixel_size):
     if fwhms_x and fwhms_y:
         avg_fwhm_x, avg_fwhm_y = np.median(fwhms_x), np.median(fwhms_y)
         ratio = np.median([fwhms_x[i] / fwhms_y[i] for i in range(len(fwhms_x))])
-        return (avg_fwhm_x + avg_fwhm_y) * pixel_size / 2, ratio, sources, avg_fwhm_x * pixel_size, avg_fwhm_y * pixel_size
-    return None, None, None, None, None
+        return (avg_fwhm_x + avg_fwhm_y) * pixel_size / 2, ratio, sources
+    return None, None, None
 
 
 def split_image_and_calculate_fwhm(image_data, pixel_size):
@@ -153,9 +151,9 @@ def split_image_and_calculate_fwhm(image_data, pixel_size):
             x_start, x_end = j * w_step, (j + 1) * w_step
             y_start, y_end = i * h_step, (i + 1) * h_step
             region_data = image_data[y_start:y_end, x_start:x_end]
-            fwhm, ratio, sources, fwhm_x, fwhm_y = calculate_fwhm(region_data, pixel_size)
+            fwhm, ratio, sources = calculate_fwhm(region_data, pixel_size)
             if fwhm and ratio:
-                fwhm_results[region_name] = {"FWHM": fwhm, "Ratio": ratio, "sources": sources, "FWHM_x": fwhm_x, "FWHM_y": fwhm_y}
+                fwhm_results[region_name] = {"FWHM": fwhm, "Ratio": ratio, "sources": sources}
             else:
                 print(f"FWHM calculation failed for {region_name}")
     return fwhm_results
