66f0e87c443c2efc461acc9d1c2ff13ab8a273d5
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-01 09:13:14 +0000

    handle the errors

--- plot_fwhm_batches.py
+++ plot_fwhm_batches.py
@@ -38,12 +38,12 @@ def load_fwhm_data_per_region(json_file):
 
 
 # Load data from JSON files for CMOS and CCD
-# load them else return None
+# Load data from JSON files for CMOS and CCD
 if args.cam == 'CMOS':
     bjds1, airmass1, fwhm_x_cmos, fwhm_y_cmos = load_fwhm_data_per_region('fwhm_results_CMOS.json')
-    bjds2, airmass2, fwhm_x_ccd, fwhm_y_ccd = 1, 1, 1, 1
+    bjds2, airmass2, fwhm_x_ccd, fwhm_y_ccd = None, None, None, None
 elif args.cam == 'CCD':
-    bjds1, airmass1, fwhm_x_cmos, fwhm_y_cmos = 1, 1, 1, 1
+    bjds1, airmass1, fwhm_x_cmos, fwhm_y_cmos = None, None, None, None
     bjds2, airmass2, fwhm_x_ccd, fwhm_y_ccd = load_fwhm_data_per_region('fwhm_results_CCD.json')
 else:
     bjds1, airmass1, fwhm_x_cmos, fwhm_y_cmos = load_fwhm_data_per_region('fwhm_results_CMOS.json')
@@ -57,31 +57,20 @@ for i in range(1, 4):
         ax = axs[i - 1, j - 1]
 
         # Calculate averages
-        avg_fwhm_x_cmos = np.mean(fwhm_x_cmos[region_name])
-        avg_fwhm_y_cmos = np.mean(fwhm_y_cmos[region_name])
-        avg_fwhm_x_ccd = np.mean(fwhm_x_ccd[region_name])
-        avg_fwhm_y_ccd = np.mean(fwhm_y_ccd[region_name])
+        avg_fwhm_x_cmos = np.mean(fwhm_x_cmos[region_name]) if fwhm_x_cmos else None
+        avg_fwhm_y_cmos = np.mean(fwhm_y_cmos[region_name]) if fwhm_y_cmos else None
+        avg_fwhm_x_ccd = np.mean(fwhm_x_ccd[region_name]) if fwhm_x_ccd else None
+        avg_fwhm_y_ccd = np.mean(fwhm_y_ccd[region_name]) if fwhm_y_ccd else None
 
         # Plot FWHM_X and Y for CMOS
-        if args.cam == 'CMOS':
+        if args.cam in ['CMOS', 'both'] and fwhm_x_cmos:
             ax.plot(bjds1, fwhm_x_cmos[region_name], 'o', label=f'FWHM_X CMOS, Avg= {avg_fwhm_x_cmos:.2f} μm', color='red',
                     markerfacecolor='none', alpha=0.3)
             ax.plot(bjds1, fwhm_y_cmos[region_name], 'o', label=f'FWHM_Y CMOS, Avg= {avg_fwhm_y_cmos:.2f} μm', color='peru',
                     markerfacecolor='none', alpha=0.3)
 
-        elif args.cam == 'CCD':
-            # Plot FWHM_Y ans X for CCD
-            ax.plot(bjds2, fwhm_x_ccd[region_name], 's', label=f'FWHM_X CCD, Avg= {avg_fwhm_x_ccd:.2f} μm', color='blue',
-                    markerfacecolor='none', alpha=0.3)
-            ax.plot(bjds2, fwhm_y_ccd[region_name], 's', label=f'FWHM_Y CCD, Avg= {avg_fwhm_y_ccd:.2f} μm', color='blueviolet',
-                    markerfacecolor='none', alpha=0.3)
-
-        # else plot both CMOS and CCD
-        else:
-            ax.plot(bjds1, fwhm_x_cmos[region_name], 'o', label=f'FWHM_X CMOS, Avg= {avg_fwhm_x_cmos:.2f} μm', color='red',
-                    markerfacecolor='none', alpha=0.3)
-            ax.plot(bjds1, fwhm_y_cmos[region_name], 'o', label=f'FWHM_Y CMOS, Avg= {avg_fwhm_y_cmos:.2f} μm', color='peru',
-                    markerfacecolor='none', alpha=0.3)
+        # Plot FWHM_X and Y for CCD
+        if args.cam in ['CCD', 'both'] and fwhm_x_ccd:
             ax.plot(bjds2, fwhm_x_ccd[region_name], 's', label=f'FWHM_X CCD, Avg= {avg_fwhm_x_ccd:.2f} μm', color='blue',
                     markerfacecolor='none', alpha=0.3)
             ax.plot(bjds2, fwhm_y_ccd[region_name], 's', label=f'FWHM_Y CCD, Avg= {avg_fwhm_y_ccd:.2f} μm', color='blueviolet',
@@ -91,7 +80,7 @@ for i in range(1, 4):
         ax.set_title(f'{region_name}', fontsize=10)
 
         # Airmass on top x-axis for only the top row plots
-        if i == 1:
+        if i == 1 and fwhm_x_cmos:
             ax2 = ax.twiny()
             ax2.set_xlim(ax.get_xlim())
             interpolated_airmass = np.interp(ax.get_xticks(), bjds1, airmass1)
